d4ee583168066decf43fe6cc9a7c53928be1c0c5
Bru <166079160+bruna-giovanella@users.noreply.github.com>
2025-09-17 19:39:58 -0300

    build: criado estrutura do projeto

---
+++ src/test/java/com/vidoe/VidoeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vidoe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VidoeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
